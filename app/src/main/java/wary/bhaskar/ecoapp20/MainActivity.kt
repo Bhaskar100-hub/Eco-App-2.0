4103ff355cd45b2cccb466f66fda56095b5b6054
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-06-21 10:17:54 +0530

    First Commit

---
+++ app/src/main/java/wary/bhaskar/ecoapp20/MainActivity.kt
@@ -0,0 +1,11 @@
+package wary.bhaskar.ecoapp20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
